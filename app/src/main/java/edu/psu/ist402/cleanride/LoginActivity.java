e30ae970b6c22af173f73e744bff8ecbd9f57f7f
gwp5008 <gwp5008@psu.edu>
2016-06-25 21:17:27 -0400

    So here is where I created a few more forms for the login and registration processes. Most of the navigation buttons work at this point. The other stuff I will look into soon concerning storing entries into the database and so forth.

---
+++ app/src/main/java/edu/psu/ist402/cleanride/LoginActivity.java
@@ -0,0 +1,39 @@
+package edu.psu.ist402.cleanride;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    public void navToHome(View view){
+        Intent goHome = new Intent(this, MainActivity.class);
+        startActivity(goHome);
+        this.finish();
+    }
+    public void navToRegister(View view){
+        Intent goToRegister = new Intent(this, RegisterActivity.class);
+        startActivity(goToRegister);
+        this.finish();
+    }
+}
--- app/src/main/java/edu/psu/ist402/cleanride/RegisterActivity.java
+++ app/src/main/java/edu/psu/ist402/cleanride/RegisterActivity.java
@@ -7,15 +7,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -33,27 +31,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(goToLogin);
         this.finish();
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void navToHome(View view){
+        Intent goHome = new Intent(this, MainActivity.class);
+        startActivity(goHome);
+        this.finish();
     }
 }
-// Initial testing to make sure that I can commit and push changes
