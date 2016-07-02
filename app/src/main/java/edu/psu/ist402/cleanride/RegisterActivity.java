cfc53c7290eb052571c99743ce5fd3de3c517f62
gwp5008 <gwp5008@psu.edu>
2016-07-02 19:23:57 -0400

    I got a little more functionality working on the register screen. I also am very close to working out the firebase database and what would be needed to store users from the registration screen. It's kind of tricky. It sure would be nice to have other people working on this with me.

--- app/src/main/java/edu/psu/ist402/cleanride/RegisterActivity.java
+++ app/src/main/java/edu/psu/ist402/cleanride/RegisterActivity.java
@@ -6,9 +6,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.firebase.client.Firebase;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String FIREBASE_URL = "https://cleanridedb.firebaseio.com/";
+    private Firebase firebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +33,54 @@ public class RegisterActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Firebase.setAndroidContext(this);
+        firebaseRef = new Firebase(FIREBASE_URL);
+
+        EditText usernameField = (EditText) findViewById(R.id.registerUserNameField);
+        EditText passwordField = (EditText) findViewById(R.id.registerPasswordField);
+        EditText emailField = (EditText) findViewById(R.id.registerEmailField);
+        EditText firstNameField = (EditText) findViewById(R.id.registerFirstNameField);
+        EditText lastNameField = (EditText) findViewById(R.id.registerLastNameField);
+        CheckBox isADriver = (CheckBox) findViewById(R.id.registerDriverBox);
+
+        BtnListener listener = new BtnListener();
+        ((Button) findViewById(R.id.registerAttemptRegisterButton)).setOnClickListener(listener);
     }
+    class BtnListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            EditText usernameInput = (EditText) findViewById(R.id.registerUserNameField);
+            EditText passwordInput = (EditText) findViewById(R.id.registerPasswordField);
+            EditText emailInput = (EditText) findViewById(R.id.registerEmailField);
+            EditText firstNameInput = (EditText) findViewById(R.id.registerFirstNameField);
+            EditText lastNameInput = (EditText) findViewById(R.id.registerLastNameField);
+            CheckBox isDriverInput = (CheckBox) findViewById(R.id.registerDriverBox);
+
+            String username = usernameInput.getText().toString();
+            String password = passwordInput.getText().toString();
+            String email = emailInput.getText().toString();
+            String firstName = firstNameInput.getText().toString();
+            String lastName = lastNameInput.getText().toString();
+            boolean isDriver = isDriverInput.isChecked();
+
+            if (!username.equals("Username") && !password.equals("Password") && !email.equals("Email")
+                    && !firstName.equals("First Name") && !lastName.equals("Last Name")){
+                RegisterHelper newUser = new RegisterHelper(username, password, email, firstName, lastName, isDriver);
+                firebaseRef.push().setValue(newUser);
+                usernameInput.setText("Username");
+                passwordInput.setText("Password");
+                emailInput.setText("Email");
+                firstNameInput.setText("First Name");
+                lastNameInput.setText("Last Name");
+                isDriverInput.setChecked(true);
+            }
+            else{
+                Snackbar.make(view, "You must enter a value into each field!", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        }
+    }
+
     public void navToLogin(View view){
         Intent goToLogin = new Intent(this, LoginActivity.class);
         startActivity(goToLogin);
